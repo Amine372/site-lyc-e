b8e6d9d15eec6ab775ffa40e592d7328be9e4af8
Amine372 <nakhila@orange.fr>
2020-09-28 13:47:03 +0200

    Update cible_inscription.php

--- traitement/cible_inscription.php
+++ traitement/cible_inscription.php
@@ -4,6 +4,8 @@
   require '../class/manager/Manager_User.php';
   session_start();
   //Vérification du mdp
+  $_POST['mdp'] = $mdp;
+  
   if($_POST['mdp'] != $_POST['confirmmdp']){
     $_SESSION['erreur_inscr'] = "Erreur dans le mot de passe.";
     header('Location: ../view/form_inscription.php');
