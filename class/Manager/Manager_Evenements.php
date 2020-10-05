14588601c06a4536c82949216dc26d4145bf1ac4
Amine372 <nakhila@orange.fr>
2020-10-05 16:49:54 +0200

    Update Manager_Evenements.php

--- class/Manager/Manager_Evenements.php
+++ class/Manager/Manager_Evenements.php
@@ -12,13 +12,13 @@ class Manager_Evenements
   public function Evenements(Evenements $Evenements)
   {
     $bdd = new PDO('mysql:host=localhost;dbname=projet_lycee','root','');
-    $req = $bdd->prepare('SELECT * from utilisateur where email = ? AND mdp = ?'); 
+    $req = $bdd->prepare('SELECT * from utilisateur where email = ? AND mdp = ?');
     $req->execute(array($connexion->getEmail(), SHA1($connexion->getMdp())));
-    $donnee = $req->fetch();// on demande enfin d'executer la requet qui a été préalablement préparée. Dans la variable donnée, on trouve les informations de la ligne qui correspond à l'uttilisateur entré et le hash du mot de passe entré. Si tant est qu'ils existebt au sein d'une meme ligne. Sinon, la variable donnée n'a pas d'affectation
-    if ($donnee) //Si la variable donnee existe, en conséquence, cela signifie que les identifiants sont valides puisque l'identifiant et le hash du mot de passe existent au sein d'une meme ligne.
+    $donnee = $req->fetch();
+    if ($donnee)
     {
-      $_SESSION['email'] = $donnee['email']; //on insère dans la session l'addresse mail entrée par l'uttilisateur dans le formulaire
-      $_SESSION['nom'] = $donnee['nom']; //on insère dans la session le non de l'uttilisateur
+      $_SESSION['email'] = $donnee['email'];
+      $_SESSION['nom'] = $donnee['nom'];
       if ($donnee['role'] == "admin")
       {
         $_SESSION['role'] = $donnee['role'];
