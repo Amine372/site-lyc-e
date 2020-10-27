aa761b6ad6cae89d04a2c0f1648598c5b1690e1a
Quentin LIGNANI <48805997+QuentinLign@users.noreply.github.com>
2020-10-27 13:58:37 +0100

    Contact

---
+++ phpmailer/cible_contact.php
@@ -0,0 +1,43 @@
+<?php
+//require './PHPMailer/src/Exception.php';
+//require './PHPMailer/src/PHPMailer.php';
+//require './PHPMailer/src/SMTP.php';
+require 'vendor/autoload.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+	$from = $_REQUEST['email'];
+    $nom = $_REQUEST['nom'];
+    $subject = $_REQUEST['subject'];
+    $message = $_REQUEST['message'];
+
+    $headers = "From: $from";
+	$headers = "From: " . $from . "\r\n";
+	$headers .= "Reply-To: ". $from . "\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+
+$mail = new PHPMailer(); // create a new object
+$mail->CharSet = 'UTF-8';
+$mail->IsSMTP(); // enable SMTP
+$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+$mail->SMTPAuth = true; // authentication enabled
+$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+$mail->Host = "smtp.gmail.com";
+$mail->Port = 465; // or 587
+$mail->IsHTML(true);
+$mail->Username = "quentin.lignani.schuman@gmail.com";
+$mail->Password = "Admwb2000";
+$mail->SetFrom("q.lignani@lprs.fr");
+$mail->Subject = "[Robert Schuman] : Réservation au Snack";
+$mail->Body = "<tr><td style='border:none;'><strong>Subject:</strong> {$nom}</td></tr>";
+$mail->AddAddress("q.lignani@lprs.fr");
+
+ if(!$mail->Send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+ } else {
+    echo "Message has been sent";
+ }
+ ?>
