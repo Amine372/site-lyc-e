2122ee05550ddb34d5ad37d3206feb18f48d7cd1
Amine372 <nakhila@orange.fr>
2020-10-27 09:27:21 +0100

    Update User.php

--- class/Modele/User.php
+++ class/Modele/User.php
@@ -1,21 +1,26 @@
 <?php
 //Modèle
-class User{
+class User
+{
 
   private $_nom;
   private $_prenom;
   private $_email;
   private $_mdp;
 
-  public function __construct(array $donnees){
+  public function __construct(array $donnees)
+  {
     $this->hydrate($donnees);
   }
 
-  public function hydrate(array $donnees){
-    foreach ($donnees as $key => $value){
+  public function hydrate(array $donnees)
+  {
+    foreach ($donnees as $key => $value)
+    {
       $method = 'set'.ucfirst($key);
 
-      if (method_exists($this, $method)){
+      if (method_exists($this, $method))
+      {
   	// On appelle le setter.
         	$this->$method($value);
       }
@@ -23,22 +28,30 @@ class User{
   }
 
   //setter
-  public function setNom($nom){
+  public function setNom($nom)
+  {
     $this->_nom = $nom;
   }
-  public function setPrenom($prenom){
+  public function setPrenom($prenom)
+  {
     $this->_prenom = $prenom;
   }
-  public function setEmail($email){
+  public function setEmail($email)
+  {
     $this->_email = $email;
   }
-  public function setMdp($mdp){
+  public function setMdp($mdp)
+  {
     $this->_mdp = $mdp;
   }
   //getter
-  public function getNom(){return $this->_nom;}
-  public function getPrenom(){return $this->_prenom;}
-  public function getEmail(){return $this->_email;}
-  public function getMdp(){return $this->_mdp;}
+  public function getNom(){return $this->_nom;
+  }
+  public function getPrenom(){return $this->_prenom;
+  }
+  public function getEmail(){return $this->_email;
+  }
+  public function getMdp(){return $this->_mdp;
+  }
 }
 ?>
