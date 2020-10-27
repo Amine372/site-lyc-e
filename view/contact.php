c85eeebe506ce020cb675a75c80bf336a5b4cd82
Amine372 <nakhila@orange.fr>
2020-10-27 16:23:23 +0100

    s

--- view/contact.php
+++ view/contact.php
@@ -122,14 +122,14 @@
 
 
 
-											
+
 											<div class="view-more">
 												<a title="">Email :</a>
-											
+
 												<u><a href="mailto:administration@lyceerobertschuman.com">administration@lyceerobertschuman.com</u>
 												<br>
 												<br>
-												<a title="">Tèl :</a>
+												<a title="">Tel :</a>
 									<u><a> 01 48 37 74 26</a></u>
 									<br>
 												<br>
@@ -143,8 +143,8 @@
 			<center><div class="main-section">
 				<div class="container">
 					<center><div class="main-section-data">
-													
-							
+
+
                       <form id="contact_form" class="contact-form" action="../phpmailer/cible_contact.php" method="post">
 
                         <div class="messages"></div>
@@ -178,18 +178,18 @@
                               </div>
                             </div>
                           </div>
-      
+
                           <input type="submit" class="buttonsend" value="Envoyer le message">
                         </div>
                       </form>
-							
-							
-					
+
+
+
 					</div></center><!-- main-section-data end-->
 				</div>
 			</div>
 		</main>
-		
+
 		<div class="container">
 <center><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2620.219618889888!2d2.4132023156387215!3d48.94930390225513!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66b9c6b0800f7%3A0xa35833adc1739e08!2sLyc%C3%A9e%20Robert%20Schuman!5e0!3m2!1sfr!2sfr!4v1603789401466!5m2!1sfr!2sfr" width="700" height="350" frameborder="0" style="border:5;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></center>
 </div>
