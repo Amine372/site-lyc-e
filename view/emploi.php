b8b88648ce0204646648e90093977b7df75aaa40
Quentin LIGNANI <48805997+QuentinLign@users.noreply.github.com>
2020-11-02 14:34:43 +0100

    Page annonces à jour

--- view/emploi.php
+++ view/emploi.php
@@ -264,11 +264,6 @@
 <script type="text/javascript" src="../js/jquery.range-min.js"></script>
 <script type="text/javascript" src="../lib/slick/slick.min.js"></script>
 <script type="text/javascript" src="../js/script.js"></script>
-<script src="assets/js/jquery.min.js"></script>
-<script src="assets/js/popper.min.js"></script>
-<script src="assets/bootstrap/js/bootstrap.min.js"></script>
-<script src="assets/js/lunar.js"></script>
-<script src="assets/js/demo.js"></script>
 </body>
 
 </html>
