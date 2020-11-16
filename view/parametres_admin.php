3d067f55044de23873b7ecdcc3ffbf69a34380ae
Amine372 <nakhila@orange.fr>
2020-11-16 15:09:03 +0100

    Update parametres_admin.php

--- view/parametres_admin.php
+++ view/parametres_admin.php
@@ -176,7 +176,7 @@
                           // on fait une boucle pour afficher tous les évenement
                           $get_event = new Manager_Evenements;
                             // $donnee doit être le tableau avec tous les evennements
-                                $donnee= $get_event->Evenements();
+                                $donnee= $get_event->Admin();
                                 if(is_null($donnee))
                                 {
                                   echo "Aucun Inscrit";
@@ -199,26 +199,12 @@
               												<ul class="descp">
               													<li><img src="images/icon8.png" alt=""></li>
               												</ul>
-              												<ul class="bk-links">
-              													<li><a href="messages.html" title=""><i class="la la-envelope"></i></a></li>
-              													<li><p title="" class="bid_now">Profil</p></li>
-              												</ul>
               											</div>
               											<div class="job_descp">
               												<h3>'.$clef['titre'].'</h3>
               												<p>'.$clef['description'].'</p>
-              												<ul class="skill-tags">
-              													<li><a href="#" title="">HTML</a></li>
-              													<li><a href="#" title="">PHP</a></li>
-              													<li><a href="#" title="">CSS</a></li>
-              													<li><a href="#" title="">Javascript</a></li>
-              													<li><a href="#" title="">Wordpress</a></li>
-              													<li><a href="#" title="">Photoshop</a></li>
-              													<li><a href="#" title="">Illustrateur</a></li>
-              													<li><a href="#" title="">Corel Draw</a></li>
-              												</ul>
               											</div>
-              										</div><!--post-bar end--> ';
+              										</div> ';
                                 }
                               }
                                 ?>
