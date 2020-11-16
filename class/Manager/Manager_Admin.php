85ebe7c11b47aca0227bbce669e051585513c716
Amine372 <nakhila@orange.fr>
2020-11-16 14:59:00 +0100

    g

--- class/Manager/Manager_Admin.php
+++ class/Manager/Manager_Admin.php
@@ -12,7 +12,7 @@ class Manager_Admin
 
       $bdd = new PDO('mysql:host=localhost;dbname=projet_lycee','root','');
       $bdd->exec("SET CHARACTER SET utf8");
-      $req = $bdd->prepare('SELECT titre, description, date, Comm, utilisateur.nom, utilisateur.prenom from evenements INNER JOIN utilisateur ON evenements.id_utilisateur = utilisateur.id ORDER BY `evenements`.`id` DESC');
+      $req = $bdd->prepare('SELECT id, nom, prenom from utilisateur ORDER BY `utilisateur`.`prenom` DESC');
       $req->execute();
       $donnee = $req->fetchall();
       if ($donnee)
