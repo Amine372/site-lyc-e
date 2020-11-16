914506231372b35d38cc2633dfa3acb26b4ea375
Amine372 <nakhila@orange.fr>
2020-11-16 14:53:21 +0100

    Update Manager_Evenements - Copie.php

--- class/Manager/Manager_Evenements - Copie.php
+++ class/Manager/Manager_Evenements - Copie.php
@@ -1,6 +1,6 @@
 <?php
-require_once(__DIR__.'/../modele/Model_Evenements.php');
-class Manager_Evenements
+require_once(__DIR__.'/../modele/Model_Admin.php');
+class Manager_Admin
 {
 
   protected $_id;
@@ -14,8 +14,6 @@ class Manager_Evenements
 
       $bdd = new PDO('mysql:host=localhost;dbname=projet_lycee','root','');
       $bdd->exec("SET CHARACTER SET utf8");
-      // SELECT * FROM Users where 0
-      // execute()
       $req = $bdd->prepare('SELECT titre, description, date, Comm, utilisateur.nom, utilisateur.prenom from evenements INNER JOIN utilisateur ON evenements.id_utilisateur = utilisateur.id ORDER BY `evenements`.`id` DESC');
       $req->execute();
       $donnee = $req->fetchall();
