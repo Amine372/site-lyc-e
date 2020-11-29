0bf8bb06c64945f4cf888c65ec329dc1748c67a1
Quentin LIGNANI <48805997+QuentinLign@users.noreply.github.com>
2020-11-29 22:05:30 +0100

    Update emploi.php

--- view/emploi.php
+++ view/emploi.php
@@ -21,8 +21,7 @@ session_start();
 <link rel="stylesheet" type="text/css" href="../css/responsive.css">
 
 
-<?php
- require '../class/Manager/manager_job.php';  ?>
+
 </head>
 
 
@@ -44,6 +43,129 @@ session_start();
           <ul>
             <li>
               <a href="../index.php" title="">
+                <span><img src="../images/icon1.png" alt=""></span>
+                Accueil
+              </a>
+            </li>
+            <li>
+              <a href="evenements.php" title="">
+                <span><img src="../images/icon3.png" alt=""></span>
+                Evénements
+              </a>
+            </li>
+            <li>
+              <a href="profils.php" title="">
+                <span><img src="../images/icon4.png" alt=""></span>
+                Profils
+              </a>
+              <ul>
+                <li><a href="profil_utilisateur.php" title="">Nom d'uttilisateur</a></li>
+                <li><a href="my-profile-feed.html" title="">L'alimentation de mon profil</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="emploi.php" title="">
+                <span><img src="../images/icon5.png" alt=""></span>
+                Annonces
+              </a>
+            </li>
+            <li>
+              <a href="contact.php" title="">
+                <span><img src="../images/icon6.png" alt=""></span>
+                Contact
+              </a>
+            </li>
+
+          </ul>
+        </nav><!--fin de la barre de navigation-->
+        <div class="menu-btn">
+          <a href="#" title=""><i class="fa fa-bars"></i></a>
+        </div><!--fin du menu bouton-->
+        <div class="user-account">
+          <?php
+          if (isset($_SESSION['nom'])) {
+            echo '<div class="user-info">
+              <img src="http://via.placeholder.com/30x30" alt="">
+              <a href="#" title="">'.$_SESSION['nom'].'</a>
+              <i class="la la-sort-down"></i>
+            </div>
+            <div class="user-account-settingss">
+              <h3>Mon Compte</h3>
+              <ul class="us-links">
+                <li><a href="parametres_du_compte.php" title="">Paramètre du compte</a></li>
+              </ul>
+              <h3 class="tc"><a href="../traitement/deconnexion.php" title="">Se déconnecter</a></h3>
+            </div><!--fin des paramètres du compte utilisateur-->';
+          }
+          else
+          {
+            echo '<div class="user-info">
+              <a href="#" title="">Connectez vous</a>
+              <i class="la la-sort-down"></i>
+            </div>
+            <div class="user-account-settingss">
+              <h3><a href="connexion.php" title="">Connexion</a></h3>
+              <h3><a href="inscription.php" title="">Inscription</a></h3>
+            </div><!--fin des paramètres du compte utilisateur-->';
+          }
+          ?>
+        </div>
+      </div><!--fin des données d'en-tête-->
+    </div>
+  </header><!--fin d'en-tête-->
+
+
+<!-- Modal Ends -->
+
+                    <div >
+                   
+    <div class="search-sec">
+      <div class="container">
+        <div class="search-box">
+          <form>
+            <input type="text" name="search" placeholder="Recherche par mots clés">
+            <button type="submit">Chercher</button>
+          </form>
+        </div><!--search-box end-->
+      </div>
+    </div><!--search-sec end-->
+ <form action="../class/Manager/manager_reservation.php" method="POST">
+
+    <div class="form-group">
+        <label for="">Votre nom et prénom</label>
+        <input type="text" name="nom" class="form-control"/ required>
+    </div>
+
+    <div class="form-group">
+        <label for="">poste</label>
+        <input type="text" name="mail" class="form-control"/ required>
+    </div>
+
+    <div class="form-group">
+        <label for="">description</label>
+        <input type="text" name="film" class="form-control"/ required>
+    </div>
+
+    <div class="form-group">
+        <label for="">Votre ville</label>
+        <input type="text" name="ville" class="form-control"/ required>
+    </div>
+
+<br>
+    <button type="submit" class="btn btn-primary">Je réserve</button>
+
+</form>
+   
+  </div>
+
+<script type="text/javascript" src="../js/jquery.min.js"></script>
+<script type="text/javascript" src="../js/popper.js"></script>
+<script type="text/javascript" src="../js/bootstrap.min.js"></script>
+<script type="text/javascript" src="../js/jquery.range-min.js"></script>
+<script type="text/javascript" src="../lib/slick/slick.min.js"></script>
+<script type="text/javascript" src="../js/script.js"></script>
+</body>
+</html>
                 <span><img src="../images/icon1.png" alt=""></span>
                 Accueil
               </a>
