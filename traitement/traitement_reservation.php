234fb238da52b1845d2918ab2e03adc8fa49dd20
Quentin LIGNANI <48805997+QuentinLign@users.noreply.github.com>
2020-11-29 22:09:12 +0100

    Update traitement_reservation.php

--- traitement/traitement_reservation.php
+++ traitement/traitement_reservation.php
@@ -1,6 +1,6 @@
 <?php
-require_once '../Modele/model_reservation.php';
-require_once '../Manager/manager_reservation.php';
+require_once '../class/Modele/model_reservation.php';
+require_once '../class/Manager/manager_reservation.php';
 $reservation = new reservation($_POST["nom"], $_POST["mail"], $_POST["film"], $_POST["ville"]);
 $co = new manager_reservation();
 $co->reservation($reservation);
