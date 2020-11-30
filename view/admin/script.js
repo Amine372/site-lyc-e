3adbc96685b2b93de6bb91004f39367ce897cc61
Amine372 <nakhila@orange.fr>
2020-11-30 15:00:36 +0100

    Update script.js

--- view/admin/script.js
+++ view/admin/script.js
@@ -1,8 +1,8 @@
-import * as monModule from '/modules/mon-module.js';
-import * as monModule from '/modules/mon-module.js';
-import * as monModule from '/modules/mon-module.js';
-import * as monModule from '/modules/mon-module.js';
-import * as monModule from '/modules/mon-module.js';
+import * as monModule from 'https://code.jquery.com/jquery-3.5.1.js';
+import * as monModule from 'https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js';
+import * as monModule from 'https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js';
+import * as monModule from 'https://cdn.datatables.net/select/1.3.1/js/dataTables.select.min.js';
+import * as monModule from 'https://editor.datatables.net/extensions/Editor/js/dataTables.editor.min.js';
 $(document).ready(function()
 {
     $('#example').DataTable(
