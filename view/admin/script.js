f6d9645e7041407c4cef1e3192b3ab70645f1b59
Amine372 <nakhila@orange.fr>
2020-11-30 14:58:50 +0100

    Update script.js

--- view/admin/script.js
+++ view/admin/script.js
@@ -1,9 +1,14 @@
+import * as monModule from '/modules/mon-module.js';
+import * as monModule from '/modules/mon-module.js';
+import * as monModule from '/modules/mon-module.js';
+import * as monModule from '/modules/mon-module.js';
+import * as monModule from '/modules/mon-module.js';
 $(document).ready(function()
 {
     $('#example').DataTable(
       {
     responsive: true
-  } 
+  }
 );
 }
 
