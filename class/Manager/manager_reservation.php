f652e7a75d432de549c341de29631483e8077c31
Quentin LIGNANI <48805997+QuentinLign@users.noreply.github.com>
2020-11-30 13:40:59 +0100

    Update manager_reservation.php

--- class/Manager/manager_reservation.php
+++ class/Manager/manager_reservation.php
@@ -1,12 +1,12 @@
 <?php
-require_once '../class/Modele/model_reservation.php';
-require_once '../traitement/traitement_reservation.php';
+require_once 'model_reservation.php';
+require_once 'traitement_reservation.php';
 class manager_reservation{ //Déclaration de la classe Manager
 public function reservation($donnee){
 
       $bdd=new PDO('mysql:host=localhost;dbname=projet_lycee;charset=utf8', 'root', ''); //Connexion à la BDD
-    $req=$bdd->prepare('INSERT into annonces (nom, mail, film, ville) VALUES(:nom, :mail, :film, :ville)'); //Préparation de la table réservations avec les valeurs de la table
-    $req->execute(array('nom'=>$donnee->getnom(),'mail'=>$donnee->getmail(), 'film'=>$donnee->getfilm(), 'ville'=>$donnee->getville())); //Execution des requêtes
+    $req=$bdd->prepare('INSERT into annonces (nom, mail, poste, ville) VALUES(:nom, :mail, :poste, :ville)'); //Préparation de la table réservations avec les valeurs de la table
+    $req->execute(array('nom'=>$donnee->getnom(),'mail'=>$donnee->getmail(), 'poste'=>$donnee->getposte(), 'ville'=>$donnee->getville())); //Execution des requêtes
     $a = $req->fetch();
 	//Conditions de redirection
     if ($a ==true){
@@ -20,4 +20,3 @@ public function reservation($donnee){
 }
 
 ?>
-
