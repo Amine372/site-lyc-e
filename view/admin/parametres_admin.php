b2904956a2993afc01f46371dd6c5ca46dbba4f1
amine372 <nakhila@orange.fr>
2020-12-06 15:19:39 +0100

    Update parametres_admin.php

--- view/admin/parametres_admin.php
+++ view/admin/parametres_admin.php
@@ -138,8 +138,6 @@
                 <th>Prénom</th>
                 <th>Role</th>
                 <th>Mail</th>
-                <th>Start date</th>
-                <th>Salary</th>
             </tr>
         </thead>
         <tbody>
@@ -148,16 +146,12 @@
                 <td>System Architect</td>
                 <td>Edinburgh</td>
                 <td>61</td>
-                <td>2011/04/25</td>
-                <td>$320,800</td>
             </tr>
             <tr>
                 <td>Garrett Winters</td>
                 <td>Accountant</td>
                 <td>Tokyo</td>
                 <td>63</td>
-                <td>2011/07/25</td>
-                <td>$170,750</td>
             </tr>
             <tr>
                 <td>Ashton Cox</td>
