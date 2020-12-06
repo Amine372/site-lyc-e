f15826cae4dc1d8be0f12fdea1ed3bd48a194a85
amine372 <nakhila@orange.fr>
2020-12-06 15:23:49 +0100

    Update parametres_admin.php

--- view/admin/parametres_admin.php
+++ view/admin/parametres_admin.php
@@ -302,72 +302,54 @@
                 <td>Regional Director</td>
                 <td>Singapore</td>
                 <td>28</td>
-                <td>2010/11/14</td>
-                <td>$357,650</td>
             </tr>
             <tr>
                 <td>Brenden Wagner</td>
                 <td>Software Engineer</td>
                 <td>San Francisco</td>
                 <td>28</td>
-                <td>2011/06/07</td>
-                <td>$206,850</td>
             </tr>
             <tr>
                 <td>Fiona Green</td>
                 <td>Chief Operating Officer (COO)</td>
                 <td>San Francisco</td>
                 <td>48</td>
-                <td>2010/03/11</td>
-                <td>$850,000</td>
             </tr>
             <tr>
                 <td>Shou Itou</td>
                 <td>Regional Marketing</td>
                 <td>Tokyo</td>
                 <td>20</td>
-                <td>2011/08/14</td>
-                <td>$163,000</td>
             </tr>
             <tr>
                 <td>Michelle House</td>
                 <td>Integration Specialist</td>
                 <td>Sydney</td>
                 <td>37</td>
-                <td>2011/06/02</td>
-                <td>$95,400</td>
             </tr>
             <tr>
                 <td>Suki Burks</td>
                 <td>Developer</td>
                 <td>London</td>
                 <td>53</td>
-                <td>2009/10/22</td>
-                <td>$114,500</td>
             </tr>
             <tr>
                 <td>Prescott Bartlett</td>
                 <td>Technical Author</td>
                 <td>London</td>
                 <td>27</td>
-                <td>2011/05/07</td>
-                <td>$145,000</td>
             </tr>
             <tr>
                 <td>Gavin Cortez</td>
                 <td>Team Leader</td>
                 <td>San Francisco</td>
                 <td>22</td>
-                <td>2008/10/26</td>
-                <td>$235,500</td>
             </tr>
             <tr>
                 <td>Martena Mccray</td>
                 <td>Post-Sales support</td>
                 <td>Edinburgh</td>
                 <td>46</td>
-                <td>2011/03/09</td>
-                <td>$324,050</td>
             </tr>
             <tr>
                 <td>Unity Butler</td>
