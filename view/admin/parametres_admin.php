62721a62357bae7d23c3dde8dd397715dc00414d
amine372 <nakhila@orange.fr>
2020-12-06 15:24:37 +0100

    Update parametres_admin.php

--- view/admin/parametres_admin.php
+++ view/admin/parametres_admin.php
@@ -356,48 +356,36 @@
                 <td>Marketing Designer</td>
                 <td>San Francisco</td>
                 <td>47</td>
-                <td>2009/12/09</td>
-                <td>$85,675</td>
             </tr>
             <tr>
                 <td>Howard Hatfield</td>
                 <td>Office Manager</td>
                 <td>San Francisco</td>
                 <td>51</td>
-                <td>2008/12/16</td>
-                <td>$164,500</td>
             </tr>
             <tr>
                 <td>Hope Fuentes</td>
                 <td>Secretary</td>
                 <td>San Francisco</td>
                 <td>41</td>
-                <td>2010/02/12</td>
-                <td>$109,850</td>
             </tr>
             <tr>
                 <td>Vivian Harrell</td>
                 <td>Financial Controller</td>
                 <td>San Francisco</td>
                 <td>62</td>
-                <td>2009/02/14</td>
-                <td>$452,500</td>
             </tr>
             <tr>
                 <td>Timothy Mooney</td>
                 <td>Office Manager</td>
                 <td>London</td>
                 <td>37</td>
-                <td>2008/12/11</td>
-                <td>$136,200</td>
             </tr>
             <tr>
                 <td>Jackson Bradshaw</td>
                 <td>Director</td>
                 <td>New York</td>
                 <td>65</td>
-                <td>2008/09/26</td>
-                <td>$645,750</td>
             </tr>
             <tr>
                 <td>Olivia Liang</td>
