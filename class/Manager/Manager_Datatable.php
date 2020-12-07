e2703bedf48211c55e433eba7cd2e0c8110196e4
Amine372 <nakhila@orange.fr>
2020-12-07 13:47:34 +0100

    Update Manager_Datatable.php

--- class/Manager/Manager_Datatable.php
+++ class/Manager/Manager_Datatable.php
@@ -1,23 +1,18 @@
 <?php
-require_once(__DIR__.'/../modele/Model_Evenements.php');
-class Manager_Evenements
+require_once(__DIR__.'/../modele/Model_Datatable.php');
+class Manager_Datatable
 {
 
   protected $_id;
-  protected $_id_utilisateur;
-  protected $_description;
-  protected $_date;
-  protected $_Comm;
+  protected $_mail;
   protected $_nom;
   protected $_prenom;
 
-  public function Evenements()
+  public function Datatable()
   {
 
       $bdd = new PDO('mysql:host=localhost;dbname=projet_lycee','root','');
       $bdd->exec("SET CHARACTER SET utf8");
-      // SELECT * FROM Users where 0
-      // execute()
       $req = $bdd->prepare('SELECT titre, description, date, Comm, utilisateur.nom, utilisateur.prenom from evenements INNER JOIN utilisateur ON evenements.id_utilisateur = utilisateur.id ORDER BY `evenements`.`id` DESC');
       $req->execute();
       $donnee = $req->fetchall();
@@ -27,42 +22,8 @@ class Manager_Evenements
       }
 
 }
-public function Admin()
-{
 
-    $bdd = new PDO('mysql:host=localhost;dbname=projet_lycee','root','');
-    $bdd->exec("SET CHARACTER SET utf8");
-    $req = $bdd->prepare('SELECT nom, prenom, id from utilisateur');
-    $req->execute();
-    $donnee = $req->fetchall();
-    if ($donnee)
-    {
-      return $donnee;
-    }
-    else
-    {
-      echo " erreur, contactez Amine, Loic ou Quentin";
-    }
-  }
 
-  public function Admin_ajout($nomprenometid)
-  {
-
-        $bdd = new PDO('mysql:host=localhost;dbname=projet_lycee','root','');
-        $bdd->exec("SET CHARACTER SET utf8");
-        $req = $bdd->prepare('UPDATE utilisateur SET role = "ADMIN" WHERE id=:id;');
-        $req->execute();
-        $donnee = $req->fetchall();
-        if ($donnee)
-        {
-          return $donnee;
-        }
-        else
-        {
-          echo " erreur, contactez Amine, Loic ou Quentin";
-        }
-
-  }
 }
 
  ?>
