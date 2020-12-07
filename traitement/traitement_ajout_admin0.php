4e362b8247e42426f5feda83a9a2f416b2f80e23
Amine372 <nakhila@orange.fr>
2020-12-07 16:44:33 +0100

    Create traitement_ajout_admin0.php

---
+++ traitement/traitement_ajout_admin0.php
@@ -0,0 +1,17 @@
+<?php
+if (isset($_POST['ajout_admin_submit']))
+{
+  $nom= $_POST['ajout_admin_nom'];
+  $prenom= $_POST['ajout_admin_prenom'];
+  $email= $_POST['ajout_admin_email'];
+  $role= $_POST['ajout_admin_role'];
+  $mdp= (SHA1($_POST['ajout_admin_mdp']));
+  $verif=0;
+  $bdd = new PDO('mysql:host=localhost;dbname=projet_lycee','root','');
+  $bdd->exec("SET CHARACTER SET utf8");
+  $req = $bdd->prepare('INSERT INTO utilisateur (nom, prenom, email, role, mdp, verif) VALUES (?, ?, ?, ?, ?, ?);');
+  $req->execute(array($nom,$prenom,$email,$role, $mdp,$verif));
+  $donnee = $req->fetchall();
+  header("Location: ../view/admin/parametres_admin.php");
+}
+?>
