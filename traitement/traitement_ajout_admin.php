fd8cf8fdbef17e97001a8a10930ac0c6f9ec9003
Amine372 <nakhila@orange.fr>
2020-12-07 14:53:07 +0100

    Update traitement_ajout_admin.php

--- traitement/traitement_ajout_admin.php
+++ traitement/traitement_ajout_admin.php
@@ -6,7 +6,7 @@
   $mdp= (SHA1($_POST['ajout_admin_mdp']));
   $verif=0;
   $bdd = new PDO('mysql:host=localhost;dbname=projet_lycee','root','');
-  $bdd->exec("SET CHARACTER SET utf8");
+  $bdd->execute(array(':calories' => 150, ':couleur' => 'red'));
   $req = $bdd->prepare('INSERT INTO utilisateur (nom, prenom, email, role, mdp, verif) VALUES (?, ?, ?, NULL, ?, ?);');
   $req->execute();
   $donnee = $req->fetchall();
