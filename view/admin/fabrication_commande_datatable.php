ee6b9d9ef0d7b196941749872c956b0b369f07f5
Amine372 <nakhila@orange.fr>
2020-12-07 13:40:50 +0100

    Update fabrication_commande_datatable.php

--- view/admin/fabrication_commande_datatable.php
+++ view/admin/fabrication_commande_datatable.php
@@ -1,21 +1,21 @@
 <?php
                     // on fait une boucle pour afficher tous les évenement
-                    $get_event = new Manager_Evenements;
+                    $get_event = new Manager_Datatable;
                       // $donnee doit être le tableau avec tous les evennements
-                          $donnee= $get_event->Evenements();
+                          $donnee= $get_event->Datatable();
                           if(is_null($donnee))
                           {
-                            echo "Aucun Inscrits";
+                            echo "Aucun Eleve";
                           }
                           else
                           {
                             foreach ($donnee as $clef)
                             {
                               echo '  <tr>
-                                  <td>'.$clef['description'].'</td>
-                                  <td>'.$clef['description'].'</td>
-                                  <td>'.$clef['description'].'</td>
-                                  <td>'.$clef['description'].'</td>
+                                  <td>'.$clef['nom'].'</td>
+                                  <td>'.$clef['prenom'].'</td>
+                                  <td>'.$clef['role'].'</td>
+                                  <td>'.$clef['mail'].'</td>
                               </tr> ';
                           }
                         }
