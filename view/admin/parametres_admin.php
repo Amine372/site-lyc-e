4116441b74eef7d37239d6a6d058e3d097029bda
Amine372 <nakhila@orange.fr>
2020-12-07 16:03:55 +0100

    Update parametres_admin.php

--- view/admin/parametres_admin.php
+++ view/admin/parametres_admin.php
@@ -103,9 +103,9 @@
                 <i class="la la-sort-down"></i>
               </div>
               <div class="user-account-settingss">
-                <h3><a href="connexion.php" title="">Connexion</a></h3>
+                <h3><a href="../connexion.php" title="">Connexion</a></h3>
 
-                <h3><a href="inscription.php" title="">Inscription</a></h3>
+                <h3><a href="../inscription.php" title="">Inscription</a></h3>
               </div><!--fin des paramètres du compte utilisateur-->';
             }
             ?>
