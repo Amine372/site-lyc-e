034d9b28b93ce33002f90f6048819e51accfd1fd
amine372 <nakhila@orange.fr>
2020-12-11 20:39:24 +0100

    Update traitement_modif_mdp.php

--- traitement/traitement_modif_mdp.php
+++ traitement/traitement_modif_mdp.php
@@ -1,19 +1,19 @@
 <?php
-	//Traitement des données entrées dans le form d'inscription
-	require '../class/modele/User.php';
-	require '../class/manager/Manager_User.php';
-	session_start();
-	//Vérification du mdp
-	if($_POST['mdp'] != $_POST['confirmmdp'])
-	{
-		$_SESSION['message_mdp'] = "Erreur dans le mot de passe.";
-		header('Location: ../view/parametres_du_compte.php');
-		exit();
-	}
+//Traitement des données entrées dans le form d'inscription
+require '../class/modele/User.php';
+require '../class/manager/Manager_User.php';
+session_start();
+//Vérification du mdp
+if($_POST['mdp'] != $_POST['confirmmdp'])
+{
+	$_SESSION['message_mdp'] = "Erreur dans le mot de passe.";
+	header('Location: ../view/parametres_du_compte.php');
+	exit();
+}
 
-	$donnee = new User(['mdp'=>$_POST['amdp']]);
+$donnee = new User(['mdp'=>$_POST['amdp']]);
 
-	$verif = new Manager_User;
-	$verif->modif_mdp($donnee, $_POST['mdp'], $_SESSION['email']);
+$verif = new Manager_User;
+$verif->modif_mdp($donnee, $_POST['mdp'], $_SESSION['email']);
 
 ?>
