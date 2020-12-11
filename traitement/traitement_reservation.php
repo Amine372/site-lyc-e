d3f23218f1eb92f977a2ee0b54a4893cc9f279ab
amine372 <nakhila@orange.fr>
2020-12-11 20:40:00 +0100

    Update traitement_reservation.php

--- traitement/traitement_reservation.php
+++ traitement/traitement_reservation.php
@@ -3,11 +3,9 @@ require_once (__DIR__.'/../class/Modele/model_reservation.php');
 require_once (__DIR__.'/../class/Manager/manager_reservation.php');
 
 
- if(isset($_POST['nom']) && $_POST['mail'] && $_POST['poste'] && $_POST['description_poste']){ //Vérifier si le mot de passe est correct
+if(isset($_POST['nom']) && $_POST['mail'] && $_POST['poste'] && $_POST['description_poste']){ //Vérifier si le mot de passe est correct
 
- 	  $reservation = new reservation($_POST["nom"], $_POST["mail"], $_POST["poste"], $_POST["description_poste"]);
- 	  $co = new manager_reservation();
-	  $co->reservation($reservation);
+  $reservation = new reservation($_POST["nom"], $_POST["mail"], $_POST["poste"], $_POST["description_poste"]);
+  $co = new manager_reservation();
+  $co->reservation($reservation);
 }
-
-
